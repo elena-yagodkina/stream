754ec7afb0ce82b51f691632334faf7fa88a482e
elena_yagodkina <engineerlena@mail.ru>
2024-03-31 11:19:17 +0300

    push_master

---
+++ src/main/java/pro/sky/stream/StreamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApplication.class, args);
+	}
+
+}
